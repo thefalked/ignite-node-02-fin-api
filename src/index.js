c9721219396d45a76c3bc0a27bbd9243cf9882f8
thefalked <giulianocrivelli@hotmail.com>
2021-07-23 00:33:54 -0400

    get statements of client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -10,22 +10,36 @@ app.use(express.json());
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
 
-  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
 
   if (customerAlreadyExists) {
-    return res.status(400).json({ error: "Customer already exists!" })
+    return res.status(400).json({ error: "Customer already exists!" });
   }
 
   const customer = {
     id: uuidv4(),
     cpf,
     name,
-    statements: []
-  }
+    statements: [],
+  };
 
   customers.push(customer);
 
   return res.status(201).json(customer);
+});
+
+app.get("/statements", (req, res) => {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(404).json({ error: "Costumer not found!" });
+  }
+
+  return res.status(200).json(customer.statements);
 });
 
 app.listen(3333);
